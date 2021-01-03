e7a1d9885c1ee4da8185d210a7b1c19d1056af3f
Cyril Wanner <info@cyr.li>
2021-01-03 23:33:02 +0000

    Bundle all classes in one index file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as Group } from './Group';
+export { default as Override } from './Override';
+export { default as Strategy } from './Strategy';
+export { default as Test } from './Test';
+export { default as TestRun } from './TestRun';
